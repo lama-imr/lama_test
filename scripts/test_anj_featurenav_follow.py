2d99dbed6c06ccca37621a2846fdcd8f1aa9d353
Gael Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2014-12-14 21:00:08 +0000

    dfs_explorer: add robustness
    git-svn-id: https://lynx1.felk.cvut.cz/svn/gael/ros/lama@346 48f9bdac-fdba-e311-b7ba-0011d8a84d3f

--- scripts/test_anj_featurenav_follow.py
+++ scripts/test_anj_featurenav_follow.py
@@ -10,16 +10,23 @@ from lama_jockeys.msg import NavigateGoal
 _edge_id = 1
 
 if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        edge_id = int(sys.argv[1])
+    else:
+        edge_id = _edge_id
     rospy.init_node('test_anj_featurenav')
 
     client = actionlib.SimpleActionClient('anj_featurenav_navigator',
                                           NavigateAction)
-    client.wait_for_server()
+    while not client.wait_for_server(rospy.Duration(5)):
+        rospy.loginfo(('waiting for action server "{}"').format(
+            'anj_featurenav_navigator'))
 
     navigate_goal = NavigateGoal()
     navigate_goal.action = navigate_goal.TRAVERSE
-    navigate_goal.edge.id = _edge_id
-    rospy.loginfo('Starting navigation')
+    navigate_goal.edge.id = edge_id
+    rospy.loginfo('Starting following segment on edge {}'.format(edge_id))
     client.send_goal_and_wait(navigate_goal)
     result = client.get_result()
 
--- scripts/test_anj_featurenav_learn.py
+++ scripts/test_anj_featurenav_learn.py
@@ -4,10 +4,49 @@
 import rospy
 import actionlib
 
+from lama_interfaces.core_interface import MapAgentInterface
+from lama_interfaces.msg import LamaObject
+from lama_interfaces.srv import ActOnMap
+from lama_interfaces.srv import ActOnMapRequest
 from lama_jockeys.msg import LearnAction
 from lama_jockeys.msg import LearnGoal
 
+
+def add_edge_to_map(descriptor_link):
+    """Add two vertices and an edge and assign the segment"""
+    iface = MapAgentInterface(start=False)
+    map_agent = rospy.ServiceProxy(iface.action_service_name,
+                                   ActOnMap)
+    rospy.loginfo('Waiting for service')
+    map_agent.wait_for_service()
+    rospy.loginfo('MapAgent ready')
+
+    edge = LamaObject()
+    edge.type = edge.EDGE
+    edge.references = [-1, -1]
+    edge_response = map_agent(object=edge, action=ActOnMapRequest.PUSH_EDGE)
+    if not edge_response.objects:
+        rospy.logerr('Database error')
+        return
+    edge_id = edge_response.objects[0].id
+    rospy.loginfo('edge {} added'.format(edge_id))
+
+    # Assign segment.
+    map_action = ActOnMapRequest()
+    map_action.action = map_action.ASSIGN_DESCRIPTOR_EDGE
+    map_action.object.id = edge_id
+    map_action.descriptor_id = descriptor_link.descriptor_id
+    map_action.interface_name = descriptor_link.interface_name
+    map_agent(map_action)
+    rospy.loginfo('segment {} assigned to edge {}'.format(
+        descriptor_link.descriptor_id, edge_id))
+
 if __name__ == '__main__':
+    import sys
+    if len(sys.argv) > 1:
+        learning_time = float(sys.argv[1])
+    else:
+        learning_time = 5
     rospy.init_node('test_anj_featurenav')
 
     client = actionlib.SimpleActionClient('anj_featurenav_learner',
@@ -18,14 +57,19 @@ if __name__ == '__main__':
     learn_goal.action = learn_goal.START_LEARN
     client.send_goal(learn_goal)
 
-    rospy.loginfo('Learning started')
+    rospy.loginfo('Learning started for {} s'.format(learning_time))
 
-    # Wait 5.
-    rospy.sleep(5)
+    # Wait for a determined time because action will never stop.
+    rospy.sleep(learning_time)
 
     learn_goal = LearnGoal()
     learn_goal.action = learn_goal.STOP_LEARN
-    client.send_goal(learn_goal)
+    client.send_goal_and_wait(learn_goal)
+    learn_result = client.get_result()
+    if not learn_result:
+        rospy.logerr('Did not receive segment descriptor')
+        exit()
+    add_edge_to_map(learn_result.descriptor_links[0])
 
     rospy.loginfo('Learning finished')
 
