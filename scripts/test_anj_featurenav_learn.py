c2bcd757fd94daafd100d2c4f09740b0fa54fb03
Gaël Ecorchard <gael.ecorchard@ciirc.cvut.cz>
2015-02-24 08:30:00 +0100

    Improve Python script for featurenav

--- scripts/test_anj_featurenav_learn.py
+++ scripts/test_anj_featurenav_learn.py
@@ -9,7 +9,10 @@ from lama_interfaces.srv import ActOnMap
 from lama_interfaces.srv import ActOnMapRequest
 from lama_jockeys.msg import LearnAction
 from lama_jockeys.msg import LearnGoal
+from lama_jockeys.msg import LearnResult
 from lama_msgs.msg import LamaObject
+
+g_has_result = False
 
 
 def add_edge_to_map(descriptor_link):
@@ -41,6 +44,14 @@ def add_edge_to_map(descriptor_link):
     rospy.loginfo('segment {} assigned to edge {}'.format(
         descriptor_link.descriptor_id, edge_id))
 
+
+def learn_done_cb(state, learn_result):
+    global g_has_result
+    if learn_result.final_state == LearnResult.DONE:
+        add_edge_to_map(learn_result.descriptor_links[0])
+        rospy.loginfo('Learning finished (jockey triggered)')
+        g_has_result = True
+
 if __name__ == '__main__':
     import sys
     rospy.init_node('test_anj_featurenav')
@@ -54,22 +65,23 @@ if __name__ == '__main__':
 
     learn_goal = LearnGoal()
     learn_goal.action = learn_goal.LEARN
-    client.send_goal(learn_goal)
+    client.send_goal(learn_goal, done_cb=learn_done_cb)
 
-    rospy.loginfo('Learning started for {} s'.format(learning_time))
+    rospy.loginfo('Learning started for at most {} s'.format(learning_time))
 
-    # Wait for a determined time because action will never stop.
+    # Wait for a determined time.
     rospy.sleep(learning_time)
 
-    learn_goal = LearnGoal()
-    learn_goal.action = learn_goal.STOP
-    client.send_goal_and_wait(learn_goal)
-    learn_result = client.get_result()
-    if not learn_result:
-        rospy.logerr('Did not receive segment descriptor')
-        exit()
-    add_edge_to_map(learn_result.descriptor_links[0])
-
-    rospy.loginfo('Learning finished')
+    # Force stop by sending a STOP goal.
+    if not g_has_result:
+        learn_goal = LearnGoal()
+        learn_goal.action = learn_goal.STOP
+        client.send_goal_and_wait(learn_goal)
+        learn_result = client.get_result()
+        if not learn_result:
+            rospy.logerr('Did not receive segment descriptor')
+            exit()
+        add_edge_to_map(learn_result.descriptor_links[0])
+        rospy.loginfo('Learning finished (high-level trigger)')
 
     rospy.spin()
